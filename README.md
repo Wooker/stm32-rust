df3aec9b54e55e8d144e127e6c6db8f5aacb6635
Zakhar Semenov <deadlion24@gmail.com>
2021-07-06 14:23:32 +0600

    minor changes

--- README.md
+++ README.md
@@ -3,18 +3,8 @@
 > Used a [template][https://github.com/rust-embedded/cortex-m-quickstart] for building applications for ARM Cortex-M microcontrollers
 developed and maintained by the [Cortex-M team][team].
 
-For the STM32F303VCT6, which has a Cortex-M3 core, I picked the `thumbv7m-none-eabi`
+For the STM32F103C8T6 which has a Cortex-M3 core, I picked the `thumbv7m-none-eabi`
 .target
-
-The next paragraphs are copied from the template:
-
-## Using this template
-
-**NOTE**: This is the very short version that only covers building programs. For
-the long version, which additionally covers flashing, running and debugging
-programs, check [the embedded Rust book][book].
-
-[book]: https://rust-embedded.github.io/book
 
 0. Before we begin you need to identify some characteristics of the target
   device as these will be used to configure the project:
@@ -32,27 +22,17 @@ programs, check [the embedded Rust book][book].
 You can find this information in the data sheet or the reference manual of your
 device.
 
-In this example we'll be using the STM32F3DISCOVERY. This board contains an
-STM32F303VCT6 microcontroller. This microcontroller has:
-
-- A Cortex-M4F core that includes a single precision FPU
-
-- 256 KiB of Flash located at address 0x0800_0000.
-
-- 40 KiB of RAM located at address 0x2000_0000. (There's another RAM region but
-  for simplicity we'll ignore it).
-
 1. Set a default compilation target. There are four options as mentioned at the
    bottom of `.cargo/config`. For the STM32F303VCT6, which has a Cortex-M4F
-   core, we'll pick the `thumbv7em-none-eabihf` target.
+   core, we'll pick the `thumbv7m-none-eabihf` target.
 
 ``` toml
 [build]
 # Pick ONE of these compilation targets
 # target = "thumbv6m-none-eabi"    # Cortex-M0 and Cortex-M0+
-# target = "thumbv7m-none-eabi"    # Cortex-M3
+target = "thumbv7m-none-eabi"    # Cortex-M3
 # target = "thumbv7em-none-eabi"   # Cortex-M4 and Cortex-M7 (no FPU)
-target = "thumbv7em-none-eabihf" # Cortex-M4F and Cortex-M7F (with FPU)
+#target = "thumbv7em-none-eabihf" # Cortex-M4F and Cortex-M7F (with FPU)
 ```
 
 2. Enter the memory region information into the `memory.x` file.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//#![deny(unused_imports)]
+//#![deny(warnings)]
 #![no_main]
 #![no_std]
 
@@ -8,20 +10,23 @@ use panic_halt as _;
 use nb::block;
 use rtic::app;
 
-use cortex_m::asm;
+//use cortex_m_semihosting::{debug, hprintln};
+
+//use cortex_m::asm;
 
 use embedded_hal::digital::v2::OutputPin;
 use stm32f1xx_hal::{
     pac::TIM2,
     timer::{Timer, CountDownTimer},
     gpio::{gpioc::PC13, Output, PushPull, State},
-    serial::{Serial, Config},
     prelude::*,
 };
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
+        #[init(1.hz())]
+        freq: i32,
         LED: PC13<Output<PushPull>>,
         //static mut DELAY: Delay                 = ();
         TIMER: CountDownTimer<TIM2>,
@@ -31,52 +36,37 @@ const APP: () = {
     fn init(cx: init::Context) -> init::LateResources{
         // Get access to the core peripherals from the cortex-m crate
         //let cp = cortex_m::Peripherals::take().unwrap();
-        // Get access to the device specific peripherals from the peripheral access crate
+        // Get access to the device specific peripherals from the peripheral
+        // access crate
         //let dp = Peripherals::take().unwrap();
-        // Take ownership over the raw flash and rcc devices and convert them into the
-        // corresponding HAL structs
+        // Take ownership over the raw flash and rcc devices and convert them
+        // into the corresponding HAL structs
         let mut rcc = cx.device.RCC.constrain();
         let mut flash = cx.device.FLASH.constrain();
 
-        let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
-        let channels = cx.device.DMA1.split(&mut rcc.ahb);
+        let _afio = cx.device.AFIO.constrain(&mut rcc.apb2);
+        let _channels = cx.device.DMA1.split(&mut rcc.ahb);
 
-        let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
+        let _gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
         let mut gpiob = cx.device.GPIOB.split(&mut rcc.apb2);
         let mut gpioc = cx.device.GPIOC.split(&mut rcc.apb2);
 
-        // Freeze the configuration of all the clocks in the system and store the 
-        // frozen frequencies in clocks`
+        // Freeze the configuration of all the clocks in the system and store the
+        // frozen frequencies in clocks
         let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-        //DELAY = Delay::new(cp.SYST, clocks);
-        //DELAY = Delay::new(device.STK, clocks);
         let t = Timer::tim2(cx.device.TIM2, &clocks, &mut rcc.apb1);
 
-        /*
-        let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
-        let rx = gpioa.pa10;
+        let _pump1 = gpiob.pb8
+            .into_push_pull_output_with_state(&mut gpiob.crh, State::High);
+        let _pump2 = gpiob.pb9
+            .into_push_pull_output_with_state(&mut gpiob.crh, State::Low);
 
 
-        let serial = Serial::usart1(
-            cx.device.USART1,
-            (tx, rx),
-            &mut afio.mapr,
-            Config::default().baudrate(9600.bps()),
-            clocks,
-            &mut rcc.apb2,
-        );
-
-        //let tx = serial.split().0.with_dma(channels.4);
-
-        //tx.write(b"The quick brown fox").wait();
-        //asm::bkpt();
-        */
-        let pump1 = gpiob.pb8.into_push_pull_output_with_state(&mut gpiob.crh, State::High);
-        let pump2 = gpiob.pb9.into_push_pull_output_with_state(&mut gpiob.crh, State::Low);
-
+        //hprintln!("init").unwrap();
         // Configure gpio C pin 13 as a push-pull output.
-        // The `crh` register is passed to the function in order to configure the port.
+        // The `crh` register is passed to the function in order to configure 
+        // the port.
         // For pins 0-7, crl should be passed instead.
         init::LateResources {
             LED: gpioc.pc13.into_push_pull_output_with_state(&mut gpioc.crh, State::High),
@@ -86,56 +76,11 @@ const APP: () = {
 
     #[idle(resources = [LED, TIMER])]
     fn idle(cx: idle::Context) -> ! {
-        /*{{{
         loop {
-            for ch in message.chars() {
-                let letter = Letter::new(ch);
-
-                // TODO: duration returns the total amount of units per letter.
-                // It should return the total amount of units per unit of Code.
-                for i in letter.code().sequence().chars() {
-                    led.set_low().unwrap();
-                    match i {
-                        '.' => {
-                            block!(timer.wait()).unwrap();
-
-                            led.set_high().unwrap();
-                            block!(timer.wait()).unwrap();
-                        }
-                        '-' => {
-                            block!(timer.wait()).unwrap();
-                            block!(timer.wait()).unwrap();
-                            block!(timer.wait()).unwrap();
-
-                            led.set_high().unwrap();
-                            block!(timer.wait()).unwrap();
-                        }
-                        ' ' => {
-                            led.set_high().unwrap();
-                            block!(timer.wait()).unwrap();
-                            block!(timer.wait()).unwrap();
-                            block!(timer.wait()).unwrap();
-                        }
-                        _ => (),
-                    };
-
-                }
-            }
-            block!(timer.wait()).unwrap();
-            block!(timer.wait()).unwrap();
-            block!(timer.wait()).unwrap();
-            block!(timer.wait()).unwrap();
-            block!(timer.wait()).unwrap();
-            block!(timer.wait()).unwrap();
-        }
-        }}}*/
-        loop {
-            //block!((*resources.TIMER).wait()).unwrap();
-            //(*resources.DELAY).delay_ms(1_000_u16);
             block!((cx.resources.TIMER).wait()).unwrap();
             (cx.resources.LED).set_high().unwrap();
-            block!((cx.resources.TIMER).wait()).unwrap();
-            (cx.resources.LED).set_low().unwrap();
+            //block!((cx.resources.TIMER).wait()).unwrap();
+            //(cx.resources.LED).set_low().unwrap();
         }
     }
 };
